554198091abb5e4cb5233e399828ab821505ca72
Hector Ballon <hector.ballon@live.com>
2018-05-06 00:00:39 -0500

    update

---
+++ Lumini.Framework/Lumini.App/Log/Log4NetLogger.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Reflection;
+using System.Xml;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
+using log4net.Repository.Hierarchy;
+using Logging = Microsoft.Extensions.Logging;
+
+namespace Lumini.App.Log
+{
+    public class Log4NetLogger : Logging.ILogger
+    {
+        private readonly ILog _log;
+        private readonly string _name;
+        private readonly XmlElement _xmlElement;
+        private readonly ILoggerRepository _loggerRepository;
+
+        public Log4NetLogger(string name, XmlElement xmlElement)
+        {
+            _name = name;
+            _xmlElement = xmlElement;
+            _loggerRepository = LogManager.CreateRepository(
+                Assembly.GetEntryAssembly(), typeof(Hierarchy));
+            _log = LogManager.GetLogger(_loggerRepository.Name, name);
+            XmlConfigurator.Configure(_loggerRepository, xmlElement);
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(Logging.LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case Logging.LogLevel.Critical:
+                    return _log.IsFatalEnabled;
+                case Logging.LogLevel.Debug:
+                case Logging.LogLevel.Trace:
+                    return _log.IsDebugEnabled;
+                case Logging.LogLevel.Error:
+                    return _log.IsErrorEnabled;
+                case Logging.LogLevel.Information:
+                    return _log.IsInfoEnabled;
+                case Logging.LogLevel.Warning:
+                    return _log.IsWarnEnabled;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(logLevel));
+            }
+        }
+
+        public void Log<TState>(Logging.LogLevel logLevel, Logging.EventId eventId, TState state,
+            Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+                return;
+
+            if (formatter == null)
+                throw new ArgumentNullException(nameof(formatter));
+            string message = null;
+            if (null != formatter)
+                message = formatter(state, exception);
+            if (!string.IsNullOrEmpty(message) || exception != null)
+                switch (logLevel)
+                {
+                    case Logging.LogLevel.Critical:
+                        _log.Fatal(message);
+                        break;
+                    case Logging.LogLevel.Debug:
+                    case Logging.LogLevel.Trace:
+                        _log.Debug(message);
+                        break;
+                    case Logging.LogLevel.Error:
+                        _log.Error(message);
+                        break;
+                    case Logging.LogLevel.Information:
+                        _log.Info(message);
+                        break;
+                    case Logging.LogLevel.Warning:
+                        _log.Warn(message);
+                        break;
+                    default:
+                        _log.Warn($"Encountered unknown log level {logLevel}, writing out as Info.");
+                        _log.Info(message, exception);
+                        break;
+                }
+        }
+    }
+}
---
+++ Lumini.Framework/Lumini.App/Log/Log4netAdapter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using log4net;
+using log4net.Config;
+using log4net.Repository.Hierarchy;
+using Lumini.Common;
+using Microsoft.Extensions.Logging;
+using ILoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
+
+namespace Lumini.App.Log
+{
+    public class Logger4NetAdapter : Common.ILogger
+    {
+        private static readonly ILog _logger =
+            LogManager.GetLogger(typeof(Logger));
+
+        static Logger4NetAdapter()
+        {
+            var log4NetConfig = new XmlDocument();
+            log4NetConfig.Load(File.OpenRead("log4net.config"));
+
+            var repo = LogManager.CreateRepository(
+                Assembly.GetEntryAssembly(), typeof(Hierarchy));
+
+            XmlConfigurator.Configure(repo, log4NetConfig["log4net"]);
+
+            var loggerFactory = (ILoggerFactory)new LoggerFactory();
+            loggerFactory.AddLog4Net();
+        }
+
+        public bool IsEnabled(LoggingEventType logLevel)
+        {
+            switch (logLevel)
+            {
+                case LoggingEventType.Debug:
+                    return _logger.IsDebugEnabled;
+                case LoggingEventType.Information:
+                    return _logger.IsInfoEnabled;
+                case LoggingEventType.Warning:
+                    return _logger.IsWarnEnabled;
+                case LoggingEventType.Error:
+                    return _logger.IsErrorEnabled;
+                case LoggingEventType.Fatal:
+                    return _logger.IsFatalEnabled;
+                default:
+                    throw new ArgumentException($"Unknown log level {logLevel}.", nameof(logLevel));
+            }
+        }
+
+        public void Log(LogEntry entry)
+        {
+            if (!IsEnabled(entry.Severity))
+            {
+                return;
+            }
+            switch (entry.Severity)
+            {
+                case LoggingEventType.Debug:
+                    _logger.Debug(entry.Message, entry.Exception);
+                    break;
+                case LoggingEventType.Information:
+                    _logger.Info(entry.Message, entry.Exception);
+                    break;
+                case LoggingEventType.Warning:
+                    _logger.Warn(entry.Message, entry.Exception);
+                    break;
+                case LoggingEventType.Error:
+                    _logger.Error(entry.Message, entry.Exception);
+                    break;
+                default:
+                    _logger.Fatal(entry.Message, entry.Exception);
+                    break;
+            }
+        }
+    }
+
+
+}
